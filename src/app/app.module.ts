5ac64a7fe697ca7faee47e95a7bcfbe46bad72be
mgoben <mitchelgoben@gojisystems.com>
2018-02-09 13:20:16 -0500

    tutorial 2 nested model driven forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
+// app.module.ts
+
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-
-import { AppComponent } from './app.component';
-
+import { AppComponent }   from './app.component';
+import { AddressComponent } from './address.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, ReactiveFormsModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
+
 export class AppModule { }
